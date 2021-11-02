815078017734df20bbbcc3e5e2f774d8abd9b90b
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-02 17:34:11 +0530

    Add Basic Logical Programme

---
+++ BasicLogicalProgramme/BasicLogicalProgramme/EvenOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicLogicalProgramme
+{
+    public class EvenOdd
+    {
+        public void getEvenOdd()
+        {
+            Console.WriteLine("Enter a num: ");
+            int userInput = int.Parse(Console.ReadLine());
+            if(userInput%2==0)
+            {
+                Console.WriteLine("Even : " + userInput);
+            }
+            else
+            {
+                Console.WriteLine("Odd : " + userInput);
+            }
+        }
+    }
+}
